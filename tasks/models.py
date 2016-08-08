07f55b50bb1618798e5d277ca7060bac4bb56cbd
Marcela Tangarife <leidy-tangarife@upc.edu.co>
2016-08-08 17:59:09 -0500

    creacion modelos tasks, angie

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,35 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class States(models.Model):
+	name_states = models.CharField(max_length=45)
+	valid = models.BooleanField(default=False)
+
+class States_kanban(models.Model):
+	name_states = models.CharField(max_length=45)
+	color = models.CharField(max_length=45)
+
+class Priorities(models.Model):
+	name_prioritie = models.CharField(max_length=45)
+	order = models.CharField(max_length=45)	
+
+class Departments(models.Model):
+	name_department = models.CharField(max_length=45)
+
+class Tasks(models.Model):
+	description = models.CharField(max_length=45)
+	answer = models.CharField(max_length=45)
+	responsable =  models.ForeignKey('people.contributors')
+	#activities =  models.ForeignKey('activities.activitie')
+	department =  models.ForeignKey('departments')
+	prioritie =  models.ForeignKey('priorities')	
+	states =  models.ForeignKey('states')	
+	#allocator =  models.ForeignKey('people.contributors')
+	start_date = models.DateTimeField(default=timezone.now)
+	finish_date = models.DateTimeField(blank=True, null=True)
+	states_kanban =  models.ForeignKey('states_kanban')
+	client =  models.ForeignKey('people.cliente')
+
+	def finish(self):
+		self.finish_date = timezone.now()
+		self.save()			
