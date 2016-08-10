dd2ecb5d625c5faefad262d00a1ee9cdedc7664f
Marcela Tangarife <leidy-tangarife@upc.edu.co>
2016-08-10 14:22:27 -0500

    url people

--- people/urls.py
+++ people/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from people.views import *
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
    
@@ -8,6 +9,7 @@ urlpatterns = [
     url(r'^view_login$',view_login, name='view_login'),
    	url(r'^view_register$',view_register, name='view_register'),
    	url(r'^register_user$', register_user, name='register_user'),
-   
-
+   	url(r'^authentication$', authentication, name='authentication'),
+   	url(r'^logout$',auth_views.logout, {'next_page':'/'}, name="logout"),
+   	url(r'^$', profile, name='profile')
 ]
--- people/views.py
+++ people/views.py
@@ -1,12 +1,37 @@
 from django.http import HttpResponse
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from django.template import loader
 from .models import Contributors
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.views.generic import ListView
+
 
 # Create your views here.
 
+def authentication(request):
+    if request.method == 'POST':
+        action = request.POST.get('action', None)
+        username = request.POST.get('username', None)
+        password = request.POSTself.get('password', None)
+
+        if action == 'signup':
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+        elif action =='login':
+            user = authenticate(username=username, password=password)
+            login(request, user)
+        return redirect('/')
+
+    return render(request, 'login.html',{})
+
+
+
+def profile(request):
+        return render_to_response('profile.html', context_instance = RequestContext(request))
+
 def view_login(request):
-     return render_to_response('login/login.html', context_instance = RequestContext(request))
+     return render_to_response('login.html', context_instance = RequestContext(request))
 
 def register_user(request):
     error = False
@@ -39,10 +64,10 @@ def Contributors(request):
 	template = loader.get_template('index.html')
 
 def view_register(request):
-	return render_to_response('view_register/view_register.html', context_instance = RequestContext(request))
+	return render_to_response('view_register.html', context_instance = RequestContext(request))
 
 def register_user(request):
-	return render_to_response('register_user/register_user.html', context_instance = RequestContext(request))
+	return render_to_response('register_user.html', context_instance = RequestContext(request))
 
 def index(request):
-	return render_to_response('templates/index.html', context_instance = RequestContext(request))
+	return render_to_response('index.html', context_instance = RequestContext(request))
