ffee17ac09c2b67b930c5c96fb70b9acda8efdc9
Julian <alka65@hotmail.com>
2016-08-17 14:48:18 -0500

    list product

--- activities/urls.py
+++ activities/urls.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^activities$',views.list_activities, name='list-activities'),#GEt
-    url('^activities/add',views.add_activity, name='add-activity'),#POST
-    url('^activities/<pk>',views.edit_activity, name='edit-activity'),#PUT
-    url('^activities/<pk>',views.delete_activity, name='delete-activity'),#DELETE
-    url('^products',views.list_products, name='list-products'),#GET
-    url('^products/add',views.add_product, name='add-product'),#POST
-    url('^products/<pk>',views.edit_product, name='edit-product'),#PUT
-    url('^products/<pk>',views.delete_product, name='delete-product'),#DELETE
+    url('^activities$',views.list_activities, name='list_activities'),#GEt
+    url('^activities/add',views.add_activity, name='add_activity'),#POST
+    url('^activities/<pk>',views.action_activity, name='edit_activity'),#PUT
+    url('^activities/<pk>',views.action_activity, name='delete_activity'),#DELETE
+    url('^products',views.list_products, name='list_products'),#GET
+    url('^products/add',views.add_product, name='add_product'),#POST
+    url('^products/<pk>',views.action_product, name='edit_product'),#PUT
+    url('^products/<pk>',views.action_product, name='delete_product'),#DELETE
 ]
--- activities/views.py
+++ activities/views.py
@@ -1,28 +1,55 @@
 from django.http import HttpResponse
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect, RequestContext, get_object_or_404
+from.models import *
+from forms import ActivitiesForm
 
+# -*- encoding: utf-8 -*-
+from unittest import result
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponseRedirect,HttpResponse
+from django.template import RequestContext
+from django.contrib.auth.hashers import make_password
+from django.contrib import auth
+from StringIO import StringIO
+from django.template.loader import render_to_string
+from models import Activities,Product
 def list_activities(request):
-     return render_to_response('../templates/activities.html')
+	activi = Activities.objects.filter()
+	return render_to_response('../templates/activities.html', {'activi': activi}, context_instance=RequestContext(request))  
 
 def add_activity(request):
+	if request.method == 'POST':
+		form = ActivitiesForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return render_to_response('../templates/activity_form.html', {'form':form}, context_instance=RequestContext(request)) 
+	else:
+		form =	ActivitiesForm()
+	return render(request, '../templates/activity_form.html', {'form':form}, context_instance=RequestContext(request))  
+     
+
+def action_activity(request):
      return render_to_response('../templates/activities.html')
 
-def edit_activity(request):
-     return render_to_response('../templates/activities.html')
-
-def delete_activity(request):
-     return render_to_response('../templates/activities.html')
 
 def list_products(request):
-     return render_to_response('../templates/activities.html')      
+	produ = Product.objects.filter()
+	return render_to_response('../templates/product.html', {'produ': produ}, context_instance=RequestContext(request))  
 
 def add_product(request):
-     return render_to_response('../templates/activities.html')
+	if request.method == 'POST':
+		prod = ProductForm(request.POST)
+		if prod.is_valid():
+			prod.save()
+			return render_to_response('../templates/product_form.html', {'prod':prod}, context_instance=RequestContext(request)) 
+     	else:
+			prod =	ProductForm()
+	return render(request, '../templates/product_form.html', {'prod':prod}, context_instance=RequestContext(request))  
 
-def edit_product(request):
-     return render_to_response('../templates/activities.html')
+def action_product(request):
+     return render_to_response('../templates/product.html')
 
-def delete_product(request):
-     return render_to_response('../templates/activities.html')
+
 
               
