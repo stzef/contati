142be1952365d45da94e74ec7d9db4a1ebaf2bb7
Julian <alka65@hotmail.com>
2016-08-24 17:57:28 -0500

    enlaces

--- activities/urls.py
+++ activities/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     url('^activities$',views.list_activities, name='list_activities'),#GEt
-    url('^activities/add',views.add_activity, name='add_activity'),#POST
-    url('^activities/<pk>',views.action_activity, name='edit_activity'),#PUT
-    url('^activities/<pk>',views.action_activity, name='delete_activity'),#DELETE
-    url('^products',views.list_products, name='list_products'),#GET
-    url('^products/add',views.add_product, name='add_product'),#POST
-    url('^products/<pk>',views.action_product, name='edit_product'),#PUT
-    url('^products/<pk>',views.action_product, name='delete_product'),#DELETE
+    url('^activities/add$',views.add_activity, name='add_activity'),#POST
+    url('^activities/<pk>$',views.action_activity, name='edit_activity'),#PUT
+    url('^activities/<pk>$',views.action_activity, name='delete_activity'),#DELETE
+    url('^products$',views.list_products, name='list_products'),#GET
+    url('^products/add$',views.add_products, name='add_products'),#POST
+    url('^products/<pk>$',views.action_product, name='edit_product'),#PUT
+    url('^products/<pk>$',views.action_product, name='delete_product'),#DELETE
 ]
