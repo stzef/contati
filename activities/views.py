10452131d72756d0ceeff2bd16cb1690cde3f27a
Julian <alka65@hotmail.com>
2016-08-25 17:37:09 -0500

    tunel

--- activities/views.py
+++ activities/views.py
@@ -34,8 +34,27 @@ def add_activity(request):
      
 
 def action_activity(request):
-  	produ = Product.objects.all()
-  	return render_to_response('../templates/edit_activity.html', {'produ': produ}, context_instance=RequestContext(request))
+  	activi = Activities.objects.all()
+
+  	if request.method == 'PUT':
+  		modelform = modelform_factory(MyModel)
+  		form = Activity_form(request.PUT)
+		if form.is_valid():
+		    form.save()
+		return redirect('list_activities')
+
+	elif request.method == 'DELETE':
+	# delete an object and send a confirmation response
+		Activities.objects.get(pk=request.DELETE['pk']).delete()
+		return HttpResponse('ok')
+
+
+  	#if request.method =='DELETE':
+  	#	id_activi = request.method.DELETE['activity_id']
+ 	#	a = Activities.objects.DELETE[pk == id_activi]
+ 	#	a.delete() 	
+ 	#	return redirect('list_activities')
+  	return render_to_response('../templates/edit_activity.html', {'activi': activi}, context_instance=RequestContext(request))
 
 def list_products(request):
 	produ = Product.objects.filter()
--- contati/settings.py
+++ contati/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'activities.middleware.HttpPostTunnelingMiddleware',
 ]
 
 ROOT_URLCONF = 'contati.urls'
