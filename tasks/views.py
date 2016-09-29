99995efc337168261725cb72b5ec8ba3420d9e60
Marcela Tangarife <leidy-tangarife@upc.edu.co>
2016-09-29 17:32:55 -0500

    cambios tareas

--- tasks/views.py
+++ tasks/views.py
@@ -68,17 +68,37 @@ class createTasks(CreateView):
 		form_kwargs["user"] = self.request.user
 		return form_kwargs
 
+	def get_context_data(self, **kwargs):
+		context = super(createTasks, self).get_context_data(**kwargs)
+		context['project'] = Projects.objects.all()
+		return context 	
+
 class editTasks(UpdateView):
 	model = Tasks
 	form_class = TasksForm
 	template_name = '../templates/edit_tasks.html'
 	success_url=reverse_lazy('list_tasks')	
 
+	def get_form_kwargs(self, **kwargs):
+		form_kwargs = super(editTasks, self).get_form_kwargs(**kwargs)
+		form_kwargs["user"] = self.request.user
+		return form_kwargs
+
+	def get_context_data(self, **kwargs):
+		context = super(createTasks, self).get_context_data(**kwargs)
+		context['project'] = Projects.objects.all()
+		return context 		
+
 class deleteTasks(DeleteView):
 	model = Tasks
 	form_class = TasksForm
 	template_name = '../templates/delete_tasks.html'
-	success_url=reverse_lazy('list_tasks')		
+	success_url=reverse_lazy('list_tasks')	
+
+	def get_context_data(self, **kwargs):
+		context = super(createTasks, self).get_context_data(**kwargs)
+		context['project'] = Projects.objects.all()
+		return context 		
 
 # <----------- View States ------------------>
 
