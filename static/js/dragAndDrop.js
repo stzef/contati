c51ffd26e7a552fa6da77023e61182b683a12beb
Julian <alka65@hotmail.com>
2016-10-12 07:41:09 -0500

    drag and drop

---
+++ static/js/dragAndDrop.js
@@ -0,0 +1,31 @@
+
+
+function star(ev) {
+  console.log(ev.target)
+ev.dataTransfer.effectAllowed = 'move';
+ev.dataTransfer.setData("text",ev.target.id);
+ev.dataTransfer.setDragImage(ev.target,80,80);
+}
+btnTarea.addEventListener("click", btnTarea);
+function btnTarea() {
+      document.body.style.backgroundColor = "red";
+}
+
+function enter(ev) {
+  return true;
+}
+
+function over(ev) {
+  ev.preventDefault();
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var datos=ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById( datos));
+  ev.stopPropagation();
+}
+
+function end(ev) {
+ev.dataTransfer.clearData("text");
+}
