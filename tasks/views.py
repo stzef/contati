35766b70c68bee298deb76c294ddc8ea0c73ab5f
Marcela Tangarife <leidy-tangarife@upc.edu.co>
2016-10-27 17:03:23 -0500

    tablero

--- tasks/views.py
+++ tasks/views.py
@@ -46,42 +46,37 @@ def view_index(request):
 
 @login_required(login_url="/login")
 def view_board(request):
-	tareas = Tasks.objects.filter()
-	form = TasksForm(user=request.user)
+    user = User.objects.get(id = request.user.id )
+    kanban1 = Tasks.objects.filter(states_kanban_id=1)
+    kanban2 = Tasks.objects.filter(states_kanban_id=2)
+    kanban3 = Tasks.objects.filter(user, states_kanban_id=3)
 
-	if request.method == "POST":
-		#import pdb; pdb.set_trace()
-		tas = Tasks()
-		tas.responsible_id = request.POST['responsible']
-		tas.description = request.POST['description']
-		tas.activity_id = request.POST['activity']
-		tas.states_id = request.POST['states']
-		tas.prioritie_id = request.POST['prioritie']
+    form = TasksForm(user=request.user)
+
+    if request.method == "POST":
+    	import pdb; pdb.set_trace()
+    	tas = Tasks()
+    	tas.responsible_id = request.POST['responsible']
+    	tas.description = request.POST['description']
+    	tas.activity_id = request.POST['activity']
+    	tas.states_id = request.POST['states']
+        tas.states_kanban_id = request.POST['states_kanban']
+    	tas.prioritie_id = request.POST['prioritie']
+        tas.save()
+        return redirect('board')
+
+	if request.method == "PUT":
+		tas = Tasks(pk=pk)
+		tas.responsible_id = request.GET['responsible']
+		tas.description = request.GET['description']
+		tas.activity_id = request.GET['activity']
+		tas.states_id = request.GET['states']
+		tas.prioritie_id = request.GET['prioritie']
 		tas.save()
 
 		return redirect('board')
 
-	return render_to_response('../templates/board.html', { 'form': form, "tareas":tareas}, context_instance=RequestContext(request) )
-
-def edit_board(request, pk):
-	tas = get_object_or_404(Tasks, pk=pk)
-	form = TasksForm(user=request.user, instance=tas)
-	if request.method == "POST":
-		#import pdb; pdb.set_trace()
-		#tas = Tasks.objects.get(pk=Tasks_id)
-		form = TasksForm(request.POST, instance=tas)
-		if form.is_valid():
-		    form.save()
-		# tas.responsible_id = request.GET['responsible']
-		# tas.description = request.GET['description']
-		# tas.activity_id = request.GET['activity']
-		# tas.states_id = request.GET['states']
-		# tas.prioritie_id = request.GET['prioritie']
-		# tas.save()
-
-		return redirect('board', pk=tas.pk)
-	return render_to_response('../templates/edit_board_task.html', { 'form': form, 'tas': tas }, context_instance=RequestContext(request) )
-
+    return render_to_response('../templates/board.html', { 'form': form, 'kanban1':kanban1, 'kanban2':kanban2, 'kanban3':kanban3 }, context_instance=RequestContext(request) )
 
 #Listar Estados
 @login_required(login_url="/login")
