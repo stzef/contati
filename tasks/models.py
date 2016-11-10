05b3af07064cbad5ab2d39ab2135cf71796b6363
Marcela Tangarife <leidy-tangarife@upc.edu.co>
2016-11-10 16:42:32 -0500

    cambios modelos

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class States(models.Model):
 	name_states = models.CharField(max_length=45)
@@ -9,10 +10,15 @@ class States(models.Model):
 
 class States_kanban(models.Model):
 	name_states = models.CharField(max_length=45)
-	color = models.CharField(max_length=45)
 
 	def __str__(self):
 		return '{}'.format(self.name_states)
+
+class Color(models.Model):
+	name_color = models.CharField(max_length=45)
+
+	def __str__(self):
+		return '{}'.format(self.name_color)
 
 class Priorities(models.Model):
 	name_prioritie = models.CharField(max_length=45)
@@ -34,9 +40,10 @@ class Tasks(models.Model):
 	department =  models.ForeignKey('departments', blank=True, null = True)
 	prioritie =  models.ForeignKey('priorities', blank=True, null = True)
 	states =  models.ForeignKey('states', blank=True, null = True)
-	start_date = models.IntegerField( default=0, blank=True, null=True)
-	finish_date = models.IntegerField( default=0, blank=True, null=True)
+	estimated_time = models.IntegerField( default=0, blank=True, null=True)
+	total_time = models.IntegerField( default=0, blank=True, null=True)
+	date_start = models.DateTimeField(blank=True, null=True)
+	date_finish = models.DateTimeField(blank=True, null=True)
 	states_kanban =  models.ForeignKey('states_kanban', blank=True, null = True)
 	activity = models.ForeignKey('activities.Activities', blank=True, null = True)
 	Customers =  models.ForeignKey('people.Customers', blank=True, null = True)
-
