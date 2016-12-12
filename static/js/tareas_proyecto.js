746c1dfd2b758132de90706bdab35f11346ed4b5
Marcela Tangarife <leidy-tangarife@upc.edu.co>
2016-12-12 11:20:36 -0500

    arreglando templates admin

--- static/js/tareas_proyecto.js
+++ static/js/tareas_proyecto.js
@@ -5,14 +5,11 @@ function seleccionTareas(pro) {
        url: '/tareas_index/'+pk,
        type: 'get',
        success : function(data){
-<<<<<<< HEAD
+
          var tar = data.tareas.fields
          console.log("-----------",tar);
          data,tareas.forEach(fields => {
-          template = '<div type="text" name="pintar" id="::idTareas::" class="formasCss" draggable="true" ondragstart="star(event)" ondragend="end(event)" style="background-color: #::hexadecimal::">'+
-              '<img class="avatar border-gray" src="/media/::fotoImagen::" width="7%" height="7% alt="foto">'+
-              '<a class="pull-right" data-toggle="modal" href="#MyModalEdit"><i class="fa fa-pencil" aria-hidden="true"></i></a>'+
-              '::descripcionTarea::'+
+          template = '<div type="text" name="pintar" id="::idTareas::">'+
           '</div>'
 
           template = template.replace("::idTareas::",task.pk).replace("::descripcionTarea::",task.fields.description).replace("::fotoImagen::",avatar).replace("::hexadecimal::",color)
@@ -22,13 +19,13 @@ function seleccionTareas(pro) {
      }
    });
  }
-=======
+
 
 function tareasPorProyecto(pro) {
   var pk = pro.value;
   console.log(pk);
   $.ajax({
-       url: '/tasks_project/'+pk,
+       url: '/tareas_index/'+pk,
        type: 'get',
        success : function(data){
          var html = ""
@@ -73,4 +70,4 @@ function tareasPorProyecto(pro) {
 });
   return error;
 };
->>>>>>> c90620c2d9be574642aadd5c36b9c09727449e41
+
