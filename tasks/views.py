92ff237a93c83a665831e3d1b79a987c6c9a5132
Marcela Tangarife <leidy-tangarife@upc.edu.co>
2016-12-14 08:10:44 -0500

    bajando cambios

--- tasks/views.py
+++ tasks/views.py
@@ -146,7 +146,6 @@ def edit_board(request, pk):
     states_kanban = request.POST.get('pos')
     tas = get_object_or_404(Tasks, pk=pk)
     if request.method == "POST":
-<<<<<<< HEAD
         form = TasksForm(request.POST, user=request.user, instance=tas)
         if form.is_valid():
                 tas = form.save(commit=False)
@@ -158,12 +157,6 @@ def edit_board(request, pk):
     else:
         form = TasksForm(user=request.user, instance=tas)
     return render_to_response('../templates/edit_board_tasks.html', { 'form':form, 'tas': tas }, context_instance=RequestContext(request) )
-=======
-        tas.states_kanban_id = states_kanban
-        tas.save()
-        return redirect('board')
-    return render_to_response('../templates/edit_board_task.html', { 'form': form, 'tas': tas }, context_instance=RequestContext(request) )
->>>>>>> a99ba8e3e119aef2abebeab05a4705536217c025
 
 @login_required(login_url="/login")
 def view_boardx4(request):
