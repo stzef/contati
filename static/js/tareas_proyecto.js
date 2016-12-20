00d670ba76ff951145ae585649225b225ce9d88d
marcela1 <leidy-tangarife@upc.edu.co>
2016-12-20 17:40:57 -0500

    correccion tareas_proyecto

--- static/js/tareas_proyecto.js
+++ static/js/tareas_proyecto.js
@@ -31,28 +31,14 @@ function seleccionTareas(seleccion) {
         var template1 = ""
         var template = '<table class="table table-bordered table-hover table-striped"><thead><th>LISTA DE TAREAS</th><th>FUNCIONES</th></thead>'
         data.tareas.forEach(fields =>{
-<<<<<<< HEAD
-          template1 =
 
-
-          '<table class="table table-bordered table-hover table-striped">'+
-          '<thead>'+
-          '<th>LISTA DE TAREAS</th><th>FUNCIONES</th>'+
-          '</thead>'+
-=======
           template1 = 
->>>>>>> eedef8dab9eca3f7dd71fcfb89f6ae10d4868c85
           '<td>::nombreTarea::</td>'+
           '<td><a class="btn btn-primary" href="/tasks/::idTareas::/edit/">Editar</a>'+
           '<a class="btn btn-danger" href="/tasks/::idTareas::/delete/">Borrar</a>'+
           '<a class="btn btn-success" href="/tasks/::idTareas::/comment/">Respuestas</a></td>'+
           '</table>'
-<<<<<<< HEAD
 
-
-=======
-             
->>>>>>> eedef8dab9eca3f7dd71fcfb89f6ae10d4868c85
           template1 = template1.replace("::nombreTarea::",fields.fields.name_task)
           .replace(/\:\:idTareas\:\:/g,fields.pk)
           html1 += template1
@@ -61,37 +47,6 @@ function seleccionTareas(seleccion) {
 
         $("#tarea").html(template + html1)
 
-<<<<<<< HEAD
-// function seleccionresponsable(seleccion) {
-//   var pk = seleccion.value;
-//   console.log(pk);
-//   $.ajax({
-//        url: '//'+pk,
-//        type: 'get',
-//        success : function(data){
-//     var html = ""
-//         var template = ""
-//         data.tareas.forEach(fields =>{
-//           template = 
-//           '<table type="text" name="seleccion" onchange="seleccionresponsable(seleccion)" class="table" id="table">'+
-//           '<tbody>'+
-//           '<h3>tareas</h3>'+'<td><label class="checkbox"><input type="checkbox" value="" data-toggle="checkbox"></label>'+
-//           '</td>'+
-//           '<td>::Tareas::</td>'+
-//           '<td class="td-actions text-right">'+
-//           '<a href="/tasks/::idTarea::/edit/" class="btn btn-info btn-simple btn-xs"><i class="fa fa-edit"></i></a>'+
-//           '<a href="/tasks/::idTarea::/delete/" class="btn btn-danger btn-simple btn-xs"><i class="fa fa-times"></i></a>'+
-//           '</td>'+
-//           '</tr>'+
-//           '</tbody>'+
-//           '</table>'
-       
-//           template = template.replace("::Tareas::",fields.fields.name_task)
-//           .replace(/\:\:idTarea\:\:/g,fields.pk)
-//           console.warn(template)
-//           html += template
-
-=======
         var html2 = ""
         var template2 = ""
         var template = '<table class="table table-bordered table-hover table-striped"><thead><th>LISTA DE TAREAS</th><th>FUNCIONES</th></thead>'
@@ -108,31 +63,16 @@ function seleccionTareas(seleccion) {
           template2 = template2.replace("::nombreactividad::",fields.fields.activity)
           .replace(/\:\:idTareas\:\:/g,fields.pk)
           html2 += template2
->>>>>>> 10b37c7217b6613bb28b5f1ff10a85f0f29e76a7
-
-//           })
-
-<<<<<<< HEAD
-//         $("#tabla").html(html)
-// }
-// });
-// };  
-=======
+        })
         $("#activity").html(template + html2)
+
 
 }
 });
-<<<<<<< HEAD
 };
-=======
-};  
->>>>>>> 10b37c7217b6613bb28b5f1ff10a85f0f29e76a7
->>>>>>> eedef8dab9eca3f7dd71fcfb89f6ae10d4868c85
 
+  
 
-
-
-<<<<<<< HEAD
 function seleccionresponsable() {
   debugger
   var user = document.getElementById('id_user');
@@ -149,41 +89,4 @@ function seleccionresponsable() {
 };  
 
 
-=======
-// function seleccionresponsable(seleccion) {
-//   var pk = seleccion.value;
-//   console.log(pk);
-//   $.ajax({
-//        url: '/tasks_ad/'+pk,
-//        type: 'get',
-//        success : function(data){
-//     var html = ""
-//         var template = ""
-//         data.tareas.forEach(fields =>{
-//           template =
-//           '<table type="text" name="seleccion" onchange="seleccionresponsable(seleccion)" class="table" id="table">'+
-//           '<tbody>'+
-//           '<h3>tareas</h3>'+'<td><label class="checkbox"><input type="checkbox" value="" data-toggle="checkbox"></label>'+
-//           '</td>'+
-//           '<td>::Tareas::</td>'+
-//           '<td class="td-actions text-right">'+
-//           '<a href="/tasks/::idTarea::/edit/" class="btn btn-info btn-simple btn-xs"><i class="fa fa-edit"></i></a>'+
-//           '<a href="/tasks/::idTarea::/delete/" class="btn btn-danger btn-simple btn-xs"><i class="fa fa-times"></i></a>'+
-//           '</td>'+
-//           '</tr>'+
-//           '</tbody>'+
-//           '</table>'
 
-//           template = template.replace("::Tareas::",fields.fields.name_task)
-//           .replace(/\:\:idTarea\:\:/g,fields.pk)
-//           console.warn(template)
-//           html += template
-
-
-//           })
-
-//         $("#tabla").html(html)
-// }
-// });
-// };
->>>>>>> a2a47108abb4631194a98ff29bea12cc4402a562
